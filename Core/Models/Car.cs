db12962643895ec86f77a636201e3e587bcc4b9a
Katarzyna <katarzyna.pabian357@gmail.com>
2023-11-18 16:03:46 +0100

    Create models

---
+++ Core/Models/Car.cs
@@ -0,0 +1,26 @@
+﻿using Core.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Core.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public string Producer { get; set; }
+        public double Capacity { get; set; }
+        public int Power { get; set; }
+        public string Motor { get; set; }
+        public string RegistrationNumber { get; set; }
+        public int OrgId { get; set; }
+        public Organization Owner { get; set; }
+        public Priority Priority { get; set; }
+    }
+    public enum Priority
+    {
+        [Display(Name = "Niski")] Low = 1,
+        [Display(Name = "Normalny")] Normal = 2,
+        [Display(Name = "Wysoki")] High = 3,
+        [Display(Name = "Pilny")] Urgent = 4
+    }
+}
---
+++ Core/Models/Organization.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Models;
+
+namespace Core.Models
+{
+    public class Organization
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string NIP { get; set; }
+        public int AdrId { get; set; }
+        public Address Address { get; set; }
+        public Car car { get; set; }
+    }
+}
